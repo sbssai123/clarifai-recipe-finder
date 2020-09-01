9bdd02583173a9e8de53e9ef6cfb348ac9cb047c
sbssai123 <sbs.sai@gmail.com>
2020-08-31 23:11:00 -0400

    env variable and endpoint return

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import multer from 'multer'
 import Clarifai from 'clarifai'
+import dotenv from 'dotenv';
 
+dotenv.config()
 const upload = multer()
-const clarifaiApp = new Clarifai.App({apiKey: "57a415e27f4c4b49879602b5409ed22e"});
+const clarifaiApp = new Clarifai.App({apiKey: process.env.API_KEY});
 
 const app = express()
 
@@ -12,7 +14,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
     const encoded = req.file.buffer.toString('base64')
     const image_byte_data = {base64: encoded}
     clarifaiApp.models.predict(Clarifai.GENERAL_MODEL, image_byte_data).then(resp => {
-        console.log(resp.rawData.outputs[0].data.concepts[0].name)
+        res.send(resp.rawData.outputs[0].data.concepts[0].name)
     })
 })
 
